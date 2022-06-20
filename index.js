e19c909c3d9ea961e9214c4565450f44724ed927
Jennifer Gifford <jgifford82@gmail.com>
2022-06-20 12:44:13 -0600

    commit tree 298e9e760c1c5269434053c0c6b8e9caf9343a16

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word.split('').reverse().join('') === word) {
+    return true
+  } else {
+    return false
+  }
 }
+
+// problem in my words:
+// The function accepts 1 argument that's a string. 
+// It checks if it's a palindrome
+// It'll return true if it is a palindrome
+// Otherwise, it'll return false
+
+// My Test Cases:
+// console.log("Expecting: true")
+// console.log("=>", isPalindrome("wow"))
+// console.log("Expecting: false")
+// console.log("=>", isPalindrome("hi"))
 
 /* 
   Add your pseudocode here
 */
+// We need to reverse the string.
+  // to reverse it, we need to use split() to return a new array.
+  // with the new array, we can use the reverse() method.
+  // then we'll need to use join() to put the reversed array back into a string
+// If the reversed string deeply equals the argument, it should return true.
+// If the reversed string does not deeply equal the argument, it should return false.
 
 /*
   Add written explanation of your solution here
@@ -13,13 +36,19 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  // console.log("Expecting: true");
+  // console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  // console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  // console.log("Expecting: false");
+  // console.log("=>", isPalindrome("robot"));
+
+
+console.log("Expecting: true");
+console.log("=>", isPalindrome("wow"));
+console.log("Expecting: false");
+console.log("=>", isPalindrome("hi"));
 }
 
 module.exports = isPalindrome;
